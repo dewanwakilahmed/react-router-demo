77aaaefc2777f4e4a269d789ecc1761c12b0b4d6
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-23 20:00:09 +0600

    Links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
